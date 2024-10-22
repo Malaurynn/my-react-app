cc53e3f765ce9185e9d38d572721394178497908
Malaurynn <malauryn@icloud.com>
2024-10-22 15:22:46 +0200

    Quête 5

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,14 +1,26 @@
-  function PokemonCard({ pokemon }) {
-  
-  
-    return (
-      <div>
-        <figure className="pokemon-card">
-          {pokemon.imgSrc ? (<img src={pokemon.imgSrc} alt={pokemon.name} />) : (<p>???</p>)}
-          <figcaption>{pokemon.name}</figcaption>
-        </figure>
-      </div>
-    );
-  }
-  
-  export default PokemonCard;
+
+interface Pokemon {
+  name: string;
+  imgSrc?: string; 
+}
+
+interface PokemonCardProps {
+  pokemon: Pokemon;
+}
+
+function PokemonCard({ pokemon }: PokemonCardProps) {
+  return (
+    <div>
+      <figure className="pokemon-card">
+        {pokemon.imgSrc ? (
+          <img src={pokemon.imgSrc} alt={pokemon.name} />
+        ) : (
+          <p>???</p>
+        )}
+        <figcaption>{pokemon.name}</figcaption>
+      </figure>
+    </div>
+  );
+}
+
+export default PokemonCard;
