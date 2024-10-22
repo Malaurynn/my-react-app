d8948adba294852c507b56436aef73aa582cdfab
Malaurynn <malauryn@icloud.com>
2024-10-22 12:08:40 +0200

    Quête 4

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,32 +1,12 @@
-const pokemonList = [
-    {
-      name: "bulbasaur",
-      imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-    },
-  ];
+  function PokemonCard({ pokemon }) {
   
-  function PokemonCard() {
-
-    const pokemon = pokemonList;
-    console.log(pokemon);
   
     return (
       <div>
-        {}
-        {pokemon.map((poke, index) => (
-          <figure key={index} className="pokemon-card">
-            {}
-            {poke.imgSrc ? (
-              <img src={poke.imgSrc} alt={poke.name} />
-            ) : (
-              <p>???</p>
-            )}
-            <figcaption>{poke.name}</figcaption>
-          </figure>
-        ))}
+        <figure className="pokemon-card">
+          {pokemon.imgSrc ? (<img src={pokemon.imgSrc} alt={pokemon.name} />) : (<p>???</p>)}
+          <figcaption>{pokemon.name}</figcaption>
+        </figure>
       </div>
     );
   }
