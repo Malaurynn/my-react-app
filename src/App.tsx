f5d53c4fc8aad0acb90a146fe399adbb645b273b
Malaurynn <167433034+Malaurynn@users.noreply.github.com>
2024-10-23 17:30:39 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,46 +2,40 @@ import "./App.css";
 import PokemonCard from './components/PokemonCard'; 
 import { useState } from "react";
 
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "charmander",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+  },
+  {
+    name: "squirtle",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+  },
+  {
+    name: "pikachu",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
 function App() {
-
   const [pokemonIndex, setPokemonIndex] = useState(0);
-
-  const pokemonList = [
-    {
-        name: "bulbasaur",
-        imgSrc:
-          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-      },
-      {
-        name: "charmander",
-        imgSrc:
-          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
-      },
-      {
-        name: "squirtle",
-        imgSrc:
-          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
-      },
-      {
-        name: "pikachu",
-        imgSrc:
-          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
-      },
-      {
-        name: "mew",
-      },
-    ];
-
-
-  const handleNext = () => {
-    setPokemonIndex((prevIndex) => (prevIndex + 1) % pokemonList.length);
+  const handleClickUp = () => {
+    setPokemonIndex(pokemonIndex + 1);
   };
-
-
-  const handlePrevious = () => {
-    setPokemonIndex((prevIndex) => (prevIndex - 1 + pokemonList.length) % pokemonList.length);
+  const handleClickDown = () => {
+    setPokemonIndex(pokemonIndex - 1);
   };
-
 
   return (
     <div>
@@ -49,12 +43,12 @@ function App() {
 
       {}
       {pokemonIndex > 0 && (
-        <button type="button" onClick={handlePrevious}>Précédent</button>
+        <button type="button" onClick={handleClickDown}>Précédent</button>
       )}
 
       {}
       {pokemonIndex < pokemonList.length - 1 && (
-        <button type="button" onClick={handleNext}>Suivant</button>
+        <button type="button" onClick={handleClickUp}>Suivant</button>
       )}
     </div>
   );
